607f297fc406614bb43c5f4e55b4c1c6673c8584
skycolor <sky@skycolor.space>
2021-11-27 03:26:21 +0300

    AudioConverter: Object ids and proper cleanup on error

--- src/Service/AudioConverter.ts
+++ src/Service/AudioConverter.ts
@@ -8,10 +8,16 @@ class AudioConverter extends BaseService {
     private logger: Logger.Logger;
     private ffmpegCmd: string | undefined;
     private inProgress: AudioConvertionInfo[];
+
+    private identifiers: WeakMap<Readable, number>;
+    private identCount: number;
+
     constructor() {
         super();
         this.logger = Logger.getLogger('audio_converter');
         this.inProgress = [];
+        this.identifiers = new WeakMap();
+        this.identCount = 0;
     }
 
     public Init(): Promise<void> {
@@ -38,7 +44,8 @@ class AudioConverter extends BaseService {
         if (!this.convertForDis) {
             throw new Error('call init() first');
         }
-    
+        this.logger.debug(this.identify(sourceStream), 'Convertion started');
+
         const child = spawn(this.ffmpegCmd, [
             '-i', 'pipe:0',
             '-vn', '-hide_banner',
@@ -52,21 +59,20 @@ class AudioConverter extends BaseService {
         ]);
     
         sourceStream.on('error', (err) => {
-            this.logger.debug('Could not convert stream', err);
-            sourceStream.unpipe();
-            pt.emit('error', err);
+            this.logger.debug(this.identify(sourceStream), 'Could not convert stream', err);
             this.abortConvertion(res);
+            pt.emit('error', err);
         });
         sourceStream.on('end', () => {
-            this.logger.debug('sourceStream ended');
+            this.logger.debug(this.identify(sourceStream), 'Stream converted (>end)');
         });
         sourceStream.on('close', () => {
-            this.logger.debug('sourceStream closed');
+            this.logger.debug(this.identify(sourceStream), 'Stream destroyed (>close)');
             this.abortConvertion(res);
         });
     
-        child.stdin.on('error', (e) => this.logger.warn('in', e));
-        child.stdout.on('error', (e) => this.logger.warn('out', e));
+        child.stdin.on('error', (e) => this.logger.warn(this.identify(sourceStream), 'in', e));
+        child.stdout.on('error', (e) => this.logger.warn(this.identify(sourceStream), 'out', e));
     
         const pt = new PassThrough({
             highWaterMark: 2 * 1024 * 1024,
@@ -81,14 +87,27 @@ class AudioConverter extends BaseService {
     }
 
     public abortConvertion(info: AudioConvertionInfo): void {
-        this.logger.info('Cleaning up streams and killing child', info.proc.pid);
-        info.inStream.unpipe();
-        info.inStream.destroy();
-        if (!info.proc.killed) {
-            info.proc.kill('SIGKILL');
+        if (this.inProgress.some(c => c === info)) {
+            this.logger.info(this.identify(info.inStream), 'Cleaning up streams and killing child', info.proc.pid);
+            info.inStream.unpipe();
+            info.inStream.destroy();
+            info.outStream.unpipe();
+            info.outStream.destroy();
+            info.proc.stdout.unpipe();
+            info.proc.stdout.destroy();
+            if (!info.proc.killed) {
+                info.proc.kill('SIGKILL');
+            }
+    
+            this.inProgress = this.inProgress.filter(c => c !== info);
         }
+    }
 
-        this.inProgress = this.inProgress.filter(c => c !== info);
+    private identify(obj: Readable): number {
+        if (!this.identifiers.has(obj)) {
+            this.identifiers.set(obj, ++this.identCount);
+        }
+        return this.identifiers.get(obj);
     }
 }
 
