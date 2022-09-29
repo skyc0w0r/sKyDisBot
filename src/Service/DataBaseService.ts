5a373331dfefa17257465e3c775a95641cf478f9
lenston19 <lenston190900@gmail.com>
2022-09-29 12:32:45 +0300

    feat(commands): added remove and change command

--- src/Service/DataBaseService.ts
+++ src/Service/DataBaseService.ts
@@ -5,7 +5,7 @@ import { Sequelize, Model, DataTypes  } from 'sequelize';
 import Logger from 'log4js';
 import { BaseCommand } from '../Model/CommandParser/index.js';
 import { UsersGif } from '../Interface/DatabaseServiceIntreface';
-
+import {PermissionsBitField} from 'discord.js';
 class DataBaseService extends BaseService {
 
     public async Init(): Promise<void> {
@@ -21,25 +21,102 @@ class DataBaseService extends BaseService {
         description: 'Subscribing to a channel join event so that the bot sends a GIF',
         options: [
             {
-                id: 'username',
-                description: 'Nickname user',
-                required: true,
+              id: 'discordId',
+              description: 'Nickname user',
+              required: true,
             },
             {
               id: 'gif',
               description: 'Link for GIF',
               required: true,
           },
-        ]
+        ],
+
+    });
+    cp.RegisterCommand('gif_change', (c) => this.gifChange(c), {
+      description: 'Change gif for user',
+      options: [
+          {
+            id: 'discordId',
+            description: 'Nickname user',
+            required: true,
+          },
+          {
+            id: 'gif',
+            description: 'Link for GIF',
+            required: true,
+        },
+      ],
+
+    });
+    cp.RegisterCommand('gif_remove', (c) => this.gifRemove(c), {
+      description: 'Remove gif for user',
+      options: [
+        {
+          id: 'discordId',
+          description: 'Nickname user',
+          required: true,
+        },
+      ],
     });
     }
     // Add new Record to DB
     private async gifAdd(cmd: BaseCommand): Promise<void> {
-      await UsersGif.create({
-        discordId: cmd.Params['username'].value,
-        gif: cmd.Params['gif'].value
-      });
-      await cmd.reply({content: 'User added successfully '});
+      if(cmd.User.permissions.has(PermissionsBitField.Flags.ManageGuild)){
+        if (!(await this.checkUserInDB(cmd.Params['discordId'].value))) {
+          await UsersGif.create({
+            discordId: cmd.Params['discordId'].value,
+            gif: cmd.Params['gif'].value
+          });
+          await cmd.reply({content: 'User added successfully '});
+        } else {
+          await cmd.reply({content: 'User exist in DB '});
+        }
+      } else {
+        await cmd.reply({content: 'Command only for moderators'});
+      }
+    }
+
+    // change gif for user
+    private async gifChange(cmd: BaseCommand): Promise<void> {
+      if(cmd.User.permissions.has(PermissionsBitField.Flags.ManageGuild)){
+        if (await this.checkUserInDB(cmd.Params['discordId'].value)) {
+          await UsersGif.update({ gif: cmd.Params['gif'].value}, {where: {
+            discordId: cmd.Params['discordId'].value
+          }
+          });
+          await cmd.reply({content: 'User updated successfully '});
+        } else {
+          await cmd.reply({content: 'The user is not in the database. Please add a user via the command "gif {user} {link gif}"'});
+        }
+      } else {
+        await cmd.reply({content: 'Command only for moderators'});
+      }
+    }
+
+      // Remove gif for user
+      private async gifRemove(cmd: BaseCommand): Promise<void> {
+        if(cmd.User.permissions.has(PermissionsBitField.Flags.ManageGuild)){
+          if (await this.checkUserInDB(cmd.Params['discordId'].value)) {
+            await UsersGif.destroy({where: {
+              discordId: cmd.Params['discordId'].value
+            }
+            });
+            await cmd.reply({content: 'User deleted successfully '});
+          } else {
+            await cmd.reply({content: 'The user is not in the database.'});
+          }
+        } else {
+          await cmd.reply({content: 'Command only for moderators'});
+        }
+      }
+
+    public async checkUserInDB(idUser: string): Promise<boolean> {
+      return await UsersGif.findOne({
+        where: {
+          discordId: idUser
+        }
+      }) ? true : false;
     }
 
     // Close connection
@@ -53,10 +130,6 @@ class DataBaseService extends BaseService {
     //   users.every(user => user instanceof UsersGif);
     //   logger.info('All users:', JSON.stringify(users, null, 2));
     // }
-
-    public async searchById(idUser: string): Promise<Model> {
-      return await UsersGif.findOne({ where: {discordId: `@${idUser}`}});
-    }
 }
 
 const logger = Logger.getLogger('main');
